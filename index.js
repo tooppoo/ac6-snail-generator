c931faf5b977f6573a237e4c6e7fed70e108e36a
philomagi <tooppoo@users.noreply.github.com>
2024-02-25 06:22:22 +0000

    add handler

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-import { badmouthes, snail } from "./src/data";
+import { generate } from './src/generator.js'
 
+const gen = document.getElementById('generate')
+const reset = document.getElementById('reset')
+const targets = document.getElementsByName('snail')
+
+gen.addEventListener('click', () => {
+    const name = generate()
+    if (!name) {
+        alert('generate() not return any value')
+
+        initialize()
+    }
+
+    setName(name)
+})
+reset.addEventListener('click', () => initialize())
+
+function initialize() {
+    setName('スネイル')
+}
+function setName(name) {
+    targets.forEach((t) => {
+        t.innerText = name
+    })
+}
+
+initialize()
